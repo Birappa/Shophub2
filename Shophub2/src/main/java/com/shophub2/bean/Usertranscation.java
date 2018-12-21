cb23bf592d0f573765800dd13d287bfe94ebb7d4
koushik <kskoushik26@gmail.com>
2018-12-21 15:02:01 +0530

    pojo of user,product and usertransaction

---
+++ Shophub2/src/main/java/com/shophub2/bean/Usertranscation.java
@@ -0,0 +1,59 @@
+package com.shophub2.bean;
+
+public class Usertranscation 
+{
+	private long userId;
+	private long productId;
+	private String productName;
+	private int quantity;
+	private double price;
+	private double total;
+	public Usertranscation(long userId, long productId, String productName,
+			int quantity, double price, double total) {
+		super();
+		this.userId = userId;
+		this.productId = productId;
+		this.productName = productName;
+		this.quantity = quantity;
+		this.price = price;
+		this.total = total;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public long getProductId() {
+		return productId;
+	}
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+
+}
