79ebe28a9858c2ee78d794e45bc6f4a099f402ab
koushik <kskoushik26@gmail.com>
2018-12-22 14:14:05 +0530

    buy now commit in cart service

--- Shophub2/src/test/java/com/shophub2/controllers/TestService.java
+++ Shophub2/src/test/java/com/shophub2/controllers/TestService.java
@@ -1,6 +1,6 @@
 package com.shophub2.controllers;
 
-import java.applet.AppletContext;
+
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,27 +11,13 @@ import com.shophub2.service.CartService;
 public class TestService 
 {
 	public static void main(String[] args) {
-		/*CartService service =new CartService();
-		service.buyNow(new UserTransaction(1, 1, "huli", 1, 1000, 1000));
-		service.buyNow(new UserTransaction(2, 1, "huli", 1, 10000, 1000));*/
-		
-		
-		ApplicationContext c= new ClassPathXmlApplicationContext("spring.xml");
-		 CartService service = (CartService) c.getBean("hi");
-			service.buyNow(new UserTransaction(1, 1, "huli", 1, 1000, 1000));
-			service.buyNow(new UserTransaction(2, 1, "huli", 1, 10000, 1000));
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
+		 CartService service = (CartService) context.getBean("hi");
+			service.buyNow(new UserTransaction(1, 1, "Headphone", 1, 1000, 1000));
+			service.buyNow(new UserTransaction(2, 1, "Earphone", 1, 10000, 1000));	
 	}
 
 }
