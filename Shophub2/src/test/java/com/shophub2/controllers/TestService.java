1982e8437ba929541346ac2e51f45fb022a9f4fd
koushik <kskoushik26@gmail.com>
2018-12-22 11:57:20 +0530

    commit buynow cartService

--- Shophub2/src/test/java/com/shophub2/controllers/TestService.java
+++ Shophub2/src/test/java/com/shophub2/controllers/TestService.java
@@ -1,14 +1,37 @@
 package com.shophub2.controllers;
 
+import java.applet.AppletContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.shophub2.bean.UserTransaction;
 import com.shophub2.service.CartService;
 
 public class TestService 
 {
 	public static void main(String[] args) {
-		CartService service =new CartService();
+		/*CartService service =new CartService();
 		service.buyNow(new UserTransaction(1, 1, "huli", 1, 1000, 1000));
-		service.buyNow(new UserTransaction(2, 1, "huli", 1, 10000, 1000));
+		service.buyNow(new UserTransaction(2, 1, "huli", 1, 10000, 1000));*/
+		
+		
+		ApplicationContext c= new ClassPathXmlApplicationContext("spring.xml");
+		 CartService service = (CartService) c.getBean("hi");
+			service.buyNow(new UserTransaction(1, 1, "huli", 1, 1000, 1000));
+			service.buyNow(new UserTransaction(2, 1, "huli", 1, 10000, 1000));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
